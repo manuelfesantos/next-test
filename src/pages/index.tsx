4b4209869d9e4343274e2d42ba3899afc48d2755
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-04-15 21:36:23 +0100

    test 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+    const [count, setCount] = useState(0);
     useEffect(() => {
-        console.log("Hello World")
+        const timer = setInterval(() => {
+            setCount(prevState => prevState + 1);
+        }, 1000);
+        return () => clearInterval(timer);
     }, []);
   return (
       <>
         <h1>Hello</h1>
           <h1>World</h1>
           <div style={{width: '100px', height: '100px', position: 'relative'}}>
-              <Image src="https://blue-heart-93dc.manuelfesantos.workers.dev/ceramics-background-1.jpeg" alt="logo" fill/>
+              <Image src="https://blue-heart-93dc.manuelfesantos.workers.dev/ceramics-background-2.jpeg" alt="logo" fill/>
           </div>
+          <h1>{count}</h1>
       </>
   );
 }
