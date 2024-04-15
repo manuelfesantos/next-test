8b52631da6dfc79e3b9ccbdb6a1bf06f3c87ab06
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-04-15 20:10:44 +0100

    test 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import {useEffect} from "react";
+
 export default function Home() {
+    useEffect(() => {
+        console.log("Hello World")
+    }, []);
   return (
       <>
         <h1>Hello</h1>
           <h1>World</h1>
+          <div style={{width: '100px', height: '100px', position: 'relative'}}>
+              <Image src="https://blue-heart-93dc.manuelfesantos.workers.dev/ceramics-background-1.jpeg" alt="logo" fill/>
+          </div>
       </>
   );
 }
